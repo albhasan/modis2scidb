77b552585eaa695d5f5c0b37c2549875702232eb
albhasan <albhasan@gmail.com>
2014-11-26 13:46:22 +0100

    Added single wrapper for GRibeiro's tool

---
+++ hdfs2sdbbin.py
@@ -0,0 +1,152 @@
+import os
+import sys
+import fnmatch
+import subprocess
+import datetime
+import argparse
+import logging
+import re
+
+
+def isLeapYear(year):
+	'''Returns TRUE if the given year (int) is leap and FALSE otherwise'''
+	leapyear = False
+	if year % 4 != 0:
+		leapyear = False
+	elif year % 100 != 0:
+		leapyear = True
+	elif year % 400 == 0:
+		leapyear = True
+	else:
+		leapyear = False
+	return leapyear
+
+
+def listFiles(path, pattern):
+	res = list()
+	for path, subdirs, files in os.walk(path):
+		for name in files:
+			filepath = os.path.join(path, name)
+			if(re.match(pattern, filepath, flags=0)):
+				res.append(filepath)
+	return res
+
+	
+def testGribModis2SciDB():
+	'''Test if GRibeiro's modis2scidb is installed'''
+	res = True
+	try:
+		subprocess.check_output(["modis2scidb", "--h"])
+	except:
+		res = False
+	return res
+
+	
+def date2grid(dateFileName, period, startyear):
+	'''Return an time index (timid) from the input date (MODIS DOY) and time period (e.g 8 days). '''
+	res = -1
+	if period > 0:
+		dateYYYYDOY = dateFileName
+		year = int(dateYYYYDOY[0:4])
+		doy = int(dateYYYYDOY[4:7])
+		ppy = int(365 / period) + 1 # Periods per year
+		if(period > 0 and (doy - 1) % period == 0):
+			idd = (doy - 1) / period
+			idy = (year - startyear) * ppy
+			res = idy + idd
+		else:
+			logging.error("date2grid: Invalid date")
+	elif period == -319980101: # Monthly - given as YYYYMMDD i.e 19980101, 19980201, 19980301
+		dateYYYYMMDD = dateFileName
+		year = int(dateYYYYMMDD[0:4])
+		mm = int(dateYYYYMMDD[4:6])
+		#dd = int(dateYYYYMMDD[6:8])
+		idy = (year - startyear) * 12
+		idd = mm - 1
+		res = idy + idd
+	return res	
+
+
+#********************************************************
+# MAIN
+#********************************************************
+def main(argv):
+	t0 = datetime.datetime.now()
+	parser = argparse.ArgumentParser(description = "Exports MODIS-HDFs to binary files for uploading to SCIDB using GRibeiro's tool")
+	parser.add_argument("hdfFolder", help = "Path to the folder containing the HDFs")
+	parser.add_argument("loadFolder", help = "Folder from where the binary files are uploaded to SCIDB")
+	parser.add_argument("product", help = "Product. e.g MOD09Q1")
+	parser.add_argument("--regex", help = "Regular expression for filtering files.", default = '^.*\.(hdf|HDF)$')
+	parser.add_argument("--log", help = "Log level. Default = WARNING", default = 'WARNING')
+	#Get paramters
+	args = parser.parse_args()
+	hdfFolder = os.path.join(args.hdfFolder, '')
+	loadFolder = os.path.join(args.loadFolder, '')
+	product = args.product
+	regex = args.regex
+	log = args.log
+	####################################################
+	# CONFIG
+	####################################################
+	prodList = ['MOD09Q1', 'MOD13Q1', 'TRMM3B43']
+	prodTemporalResolution = {
+		'MOD09Q1':	8,
+		'MOD13Q1':	16,
+		'TRMM3B43':	-319980101
+	}
+	prodStartYear = {
+		'MOD09Q1':	2000,
+		'MOD13Q1':	2000,
+		'TRMM3B43':	1998
+	}
+	prodBands = {
+		'MOD09Q1':	'0,1,2',
+		'MOD13Q1':	'0,1,2,3,4,5,6,7,8,9,10,11',
+		'TRMM3B43':	'0,1,2'
+	}
+	numeric_loglevel = getattr(logging, log.upper(), None)
+	if not isinstance(numeric_loglevel, int):
+		raise ValueError('Invalid log level: %s' % log)
+	logging.basicConfig(filename = 'log_hdfs2sdbin.log', level = numeric_loglevel, format = '%(asctime)s %(levelname)s: %(message)s')
+	logging.info("log_hdfs2sdbin.py: " + str(args))
+	####################################################
+	# VALIDATION
+	####################################################
+	if product in prodList == False:
+		logging.exception("Unknown product!")
+		raise Exception("Unknown product!")
+	if testGribModis2SciDB() == False:
+		logging.exception("GRibeiro's mod2scidb not found")
+		raise Exception("GRibeiro's mod2scidb not found")
+	####################################################
+	# 
+	####################################################
+	period = prodTemporalResolution[product]
+	startyear = prodStartYear[product]
+	bands = prodBands[product]
+	hdfs = listFiles(hdfFolder, regex)
+	for i in range(0, len(hdfs)) :
+		hdf = hdfs[i]
+		filename = os.path.basename(hdf)
+		time_id = date2grid(filename.split(".")[1][1:], period, startyear)
+		arg0 = "modis2scidb"
+		arg1 = " --f " + hdf
+		arg2 = " --o " + loadFolder + os.path.splitext(filename)[0] + ".sdbbin"
+		arg3 = " --b " + bands
+		arg4 = " --t " + str(time_id)
+		cmd = arg0 + arg1 + arg2 + arg3 + arg4
+		if(i != len(hdfs)):
+			cmd = cmd + " & "
+		logging.info(cmd)
+		subprocess.check_call(str(cmd), shell = True)
+	t1 = datetime.datetime.now()	
+	tt = t1 - t0
+	logging.info("Finished in " + str(tt))
+	
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   
+   
+   
+   
